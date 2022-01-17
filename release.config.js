020a9cb4ad4d792cae66b94927c26dfd79be5d73
George Spake <george@georgespake.com>
2022-01-16 21:43:40 -0600

    build(npm-demo): update release config

--- release.config.js
+++ release.config.js
@@ -2,9 +2,14 @@ module.exports = {
     "plugins": [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        ["@semantic-release/git", {
-            "assets": ["CHANGELOG.md", "package.json"],
-            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-        }]
-    ]
+        "@semantic-release/changelog",
+        ["@semantic-release/npm", {
+            "tarballDir": "release"
+        }],
+        ["@semantic-release/github", {
+            "assets": "release/*.tgz"
+        }],
+        "@semantic-release/git"
+    ],
+    "preset": "angular"
 }
